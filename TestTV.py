52982d1ba52e22c7dbecd1c0a4445f5a900c5479
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-05-19 18:13:23 +0800

    Add tv2 in TestTV

--- TestTV.py
+++ TestTV.py
@@ -3,3 +3,8 @@ tv1 = TV()
 tv1.setChannel(30)
 tv1.setVolume(3)
 print ("tv1's channel is", tv1.getChannel(), "and volume level is", tv1.getVolume())
+
+tv2 = TV()
+tv2.setChannel(3)
+tv2.setVolume(2)
+print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
