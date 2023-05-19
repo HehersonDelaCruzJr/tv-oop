d859487dcb01c5a6eda6efef257992098fec6bf4
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-05-19 18:09:14 +0800

    add Channel up and down, and Volume Up and Down

--- TV.py
+++ TV.py
@@ -23,3 +23,24 @@ class TV:
         
     def turnOff(self):
         self.on = False
+        
+    def channelUp(self):
+        if self.channel<120:
+            self.channel += 1
+        else:
+            self.channel =1
+    def ChannelDown(self):
+        if self.channel >1:
+            self.channel -= 1
+        else:
+            self.channel = 120
+    def VolumeUp(self):
+        if self.channel <7:
+            self.channel += 1
+        else:
+            self.channel = 1
+    def VolumeDown(self):
+        if self.channel >1:
+            self.channel -= 1
+        else:
+            self.channel = 7
